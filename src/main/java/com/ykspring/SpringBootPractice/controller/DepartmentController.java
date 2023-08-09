9c104a8da29a3a26e8f8543c9d9388a00ecf73f9
yogeshkumar17 <yk724664@gmail.com>
2023-08-09 21:50:16 +0530

    Loggers ------ 9 August, 2023

--- src/main/java/com/ykspring/SpringBootPractice/controller/DepartmentController.java
+++ src/main/java/com/ykspring/SpringBootPractice/controller/DepartmentController.java
@@ -2,9 +2,12 @@ package com.ykspring.SpringBootPractice.controller;
 
 import com.ykspring.SpringBootPractice.entity.Department;
 import com.ykspring.SpringBootPractice.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -12,35 +15,44 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment( @Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> getDepartments(){
+        LOGGER.info("Inside getDepartment of DepartmentController");
         return departmentService.getDepartments();
     }
 
     @GetMapping("/departments/{id}")
     public Department getDepartmentById( @PathVariable("id") Long departmentId){
+        LOGGER.info("Inside getDepartmentById of DepartmentController");
         return departmentService.getDepartmentById(departmentId);
     }
 
     @DeleteMapping("departments/{id}")
     public String deleteDepartmentById( @PathVariable("id") Long departmentId){
+        LOGGER.info("Inside deleteDepartmentById of DepartmentController");
         departmentService.deleteDepartmentById(departmentId);
         return "Department deleted successfully!";
     }
 
     @PutMapping("departments/{id}")
-    public Department updateDepartmentById( @RequestBody Department department, @PathVariable("id") Long departmentId){
+    public Department updateDepartmentById( @Valid @RequestBody Department department, @PathVariable("id") Long departmentId){
+        LOGGER.info("Inside updateDepartmentById of DepartmentController");
         return departmentService.updateDepartmentById(department, departmentId);
     }
 
 
     @GetMapping("departments/name/{name}")
     public Department fetchDepartmentByName( @PathVariable("name") String departmentName){
+        LOGGER.info("Inside fetchDepartmentByName of DepartmentController");
         return departmentService.fetchDepartmentByName(departmentName);
     }
 
--- src/main/java/com/ykspring/SpringBootPractice/entity/Department.java
+++ src/main/java/com/ykspring/SpringBootPractice/entity/Department.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 public class Department {
@@ -63,6 +65,7 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long DepartmentId;
+    @NotBlank(message = "Please add department name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
